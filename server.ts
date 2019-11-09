f0f18b3ad668d05dadcac47a6e032ed5803dde75
najmalrawi <najmalrawi@gmail.com>
2019-11-09 14:14:34 +0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -6,8 +6,10 @@ import { default as options } from "./server/server-options"
 const server = new GraphQLServer({ typeDefs, resolvers })
 
 server.start(options, () => {
+
 	console.log(`Server is running on localhost:${options.port}`)
 
 }).catch(error => {
+
 	console.error("Connection error", error)
 })
